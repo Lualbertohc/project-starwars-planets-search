8095074f32ee6fef845c190aa2a159fd265a9a0b
Luis Alberto H. Cardoso <luis.alberto3556@gmail.com>
2022-10-26 14:13:25 -0300

    primeiro requiso refatorado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import Provider from './context/Provider';
 
@@ -8,8 +7,6 @@ ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
     <Provider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>,
   );
